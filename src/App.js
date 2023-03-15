7237bbc07d1e2aa6173f23456dc97ce3596f6e2e
Shashank Varshney <varshneyshashank8846@gmail.com>
2023-03-15 12:55:24 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignIn from './Components/SignIn';
+import Dashboard from './Components/Dashboard';
 function App() {
   return (
     <>
     <Router>
       <Routes>
          <Route element={<SignIn/>} path='/'></Route>
+          <Route element={<Dashboard/>} path='/dashboard'></Route>
       </Routes>
     </Router>
     </>
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Skills from './Skills'
+import Timesheet from './Timesheet'
+
+function Dashboard() {
+  return (
+    <>
+    <div id='dashboard'>
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-1">
+                        Celebal
+                    </div>
+                
+                <div className="col-md-11">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <input type="text" name="" id="" className='form-control'/>
+                        </div>
+                    </div>
+                    <hr />
+                <div className="row w-100 mt-3">
+                    <div className="col-md-6 ">
+                        <Skills/>
+                    </div>
+                    <div className="col-md-6">
+                        <Timesheet/>
+                    </div>
+                </div>
+                <div className="row w-100 mt-3">
+                    <div className="col-md-6 ">
+                        <Skills/>
+                    </div>
+                    <div className="col-md-6">
+                        <Timesheet/>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
