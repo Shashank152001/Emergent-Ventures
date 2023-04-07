31e081271bd969a26e845fc74d0720507f49e3e0
Shashank Varshney <varshneyshashank8846@gmail.com>
2023-04-07 17:19:34 +0530

    profile code update

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,99 +1,185 @@
 import React, { useState, useEffect } from "react";
 import "./Profile.css";
-
+import Sidebar from "../Dashboard/sidebar";
+import Header from "../Dashboard/Header";
 
 function Profile() {
- 
-    return (
-        <>
-            <div className="main">
-                <div className="container">
-                    <div className="form-wrapper">
-                        <div className="formDetail-container">
-                            <h4 className="personal-text">Personal Details</h4>
-                            <form >
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">First Name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels"> Surname</label>
-                                        <input type="text" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">Departure Date</label>
-                                        <input type="date" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels">Return Date</label>
-                                        <input type="date" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}} />
-                                    </div>
-                                </div>
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels"> name</label>
-                                        <input type="text" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels"> name</label>
-                                        <input type="text" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div class="col">
-                                        <label className="labels"> name</label>
-                                        <input type="text" class="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels"> name</label>
-                                        <input type="text" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                 <div className="row name-form">
-                                    <div className="col">
-                                        <label className="labels">name</label>
-                                        <input type="text" className="form-control" placeholder="First name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                    <div className="col">
-                                        <label className="labels"> name</label>
-                                        <input type="text" className="form-control" placeholder="Last name" style={{backgroundColor:'#EAEAEA'}}/>
-                                    </div>
-                                </div>
-                                   <div className="name-form">
-                                   <button className="btn btn-primary" style={{width:'15%',height:'50px'}}>Submit</button>
-                                    </div> 
-                                
-                            </form>
-
-                        </div>
-                    </div>
-
+  return (
+    <>
+      <div className="main-container">
+        <div className="wrapper d-flex">
+          <Sidebar />
+          <div
+            className="right-sidebar"
+            style={{
+              position: "absolute",
+              width: "87%",
+              height: "100%",
+              right: "0",
+            }}
+          >
+            <Header />
+          </div>
+        </div>
+        <div className="container">
+          <div className="form-wrapper">
+            <div className="formDetail-container">
+              <h4 className="personal-text">Personal Details</h4>
+              <form>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">First Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels"> Surname</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
                 </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">Departure Date</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels">Return Date</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels"> name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels"> name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div class="col">
+                    <label className="labels"> name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels"> name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="row name-form">
+                  <div className="col">
+                    <label className="labels">name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="First name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                  <div className="col">
+                    <label className="labels"> name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Last name"
+                      style={{ backgroundColor: "#EAEAEA" }}
+                    />
+                  </div>
+                </div>
+                <div className="name-form">
+                  <button
+                    className="btn btn-primary"
+                    style={{ width: "15%", height: "40px" }}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Profile;
--- src/Constant/Url.js
+++ src/Constant/Url.js
@@ -1,6 +1,6 @@
 
-export const url='https://palm-vpn-headline-nathan.trycloudflare.com/';
-// export const url='https://3553-117-242-153-226.in.ngrok.io/';
+// export const url='https://beverages-frontpage-submissions-furnished.trycloudflare.com/';
+export const url='https://11ae-117-242-153-226.in.ngrok.io/';
 
 
 
