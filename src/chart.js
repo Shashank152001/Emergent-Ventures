b7d4eec7872dd7c7190d34c8895d7b2fe9618492
karuna2000 <karunay303@gmail.com>
2023-04-27 18:21:26 +0530

    files modified

--- src/chart.js
+++ src/chart.js
@@ -1,42 +1,77 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import OrgChart from "@balkangraph/orgchart.js";
 
-const Chart = (props) => {
+const url = 'https://nerve-calculation-mario-excluded.trycloudflare.com/user/get-user-hierarchy';
+
+
+const Chart = () => {
+
   const currentRef = useRef();
+
+  const[data,setData] = useState(null);
+
+
+  useEffect(()=>{
+
+    fetch(url,{
+      method:'GET',
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    }).then((res)=>res.json()).then((data)=>{
+      // console.log(data);
+      setData(data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+
+  },[])
 
   useEffect(() => {
 
     OrgChart.templates.ana.plus = "";
     OrgChart.templates.ana.minus = "";
 
-    new OrgChart(currentRef.current, {
-      nodes: props.nodes,
+
+    if(data){
+
+   const chart =  new OrgChart(currentRef.current, {
+     
+      nodes: data,
       enableSearch: false,
       mouseScrool: OrgChart.action.none,
       nodeMouseClick: OrgChart.action.none,
+      toolbar: {
+        zoom: true,
+        fit: true,
+      },
 
       tags: {
         "node-with-subtrees": {
-            template: "group",
-            subTreeConfig: {
-                siblingSeparation: 10,
-                columns: 3
-            }
-        }
-    },
-    
+          template: "group",
+          subTreeConfig: {
+            siblingSeparation: 10,
+            columns: 3,
+          },
+        },
+      },
+
       nodeBinding: {
         field_0: "name",
-        img_0: "img",
+        field_1: "role",
+        img_0: "profileImage",
       },
     });
-  }, []);
+
+   
+  }
+
+  }, [data]);
 
   return (
     <div
       id="tree"
       ref={currentRef}
-      style={{ height: "100vh", backgroundColor: "#ff6e6e" }}
+      style={{ height: "100vh"}}
     ></div>
   );
 };
