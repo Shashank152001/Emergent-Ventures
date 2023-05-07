d6ed4c4ef8c3c563b7ad24933490ce342ed76849
Shashank Varshney <varshneyshashank8846@gmail.com>
2023-05-07 08:41:27 +0530

    taking pull for timesheet

--- src/Constant/Url.js
+++ src/Constant/Url.js
@@ -1,6 +1,6 @@
 
 // export const url = 'https://safari-craig-calls-ns.trycloudflare.com/'
-export const url = 'https://shows-consultants-tee-knit.trycloudflare.com/'
+export const url = 'https://caroline-surgeons-pose-session.trycloudflare.com/'
 // export const url = "http://localhost:5000/"
 
 
