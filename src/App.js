096d77d5283a8a3810b9d832faf0e1f4744938c7
Shashank Varshney <varshneyshashank8846@gmail.com>
2023-06-07 15:36:21 +0530

    admin route protect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import {LoginContext,RealDataContext} from './Context/LoginContext'
+import { LoginContext, RealDataContext } from "./Context/LoginContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,42 +10,37 @@ import Profile from "./Components/Profile/Profile";
 import GetProfile from "./Components/Profile/getProfile";
 import EmployeeTable from "./Components/EmployeeTable/EmployeeTable";
 import Dashboard from "./Components/Dashboard/Dashboard";
-import ProtectRoute from "./Service/ProtectRoute";
+import { ProtectRoute, AdminProtect } from "./Service/ProtectRoute";
 import TimesheetForm from "./Components/Timesheet/TimesheetForm";
-import RedirectRoute from "./Service/RedirectRoute";
+import { RedirectRoute, AdminRedirect } from "./Service/RedirectRoute";
 import ErrorPage from "./Components/ErrorComponet/ErrorPage";
 import LeaveForm from "./Components/Leaves/leaveform";
 import Home from "./Components/Dashboard/home";
-import Gethierarchy from './Components/Profile/Gethierarchy'
+import Gethierarchy from "./Components/Profile/Gethierarchy";
 import GetRequest from "./Components/WFH/GetRequest";
 import ViewRequest from "./Components/WFH/ViewRequest";
-import WFHform from './Components/WFH/WFHform'
+import WFHform from "./Components/WFH/WFHform";
 import EditRequest from "./Components/WFH/EditRequest";
 import GetUserTimesheet from "./Components/Timesheet/GetUserTimesheet";
 import GetRmTimesheet from "./Components/Timesheet/GetRmTimesheet";
 import EditReporingTimeSheet from "./Components/Timesheet/EditReportingTimeSheet";
 import SearchProfile from "./Components/Profile/SearchProfile";
-import DescriptionForm from './Components/Timesheet/descriptionForm'
+import DescriptionForm from "./Components/Timesheet/descriptionForm";
 import AdminLogin from "./Components/AdminLogin/AdminLogin";
 import AdminSignUp from "./Components/AdminSignUp/AdminSignUp";
 import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
 
-
-
 function App() {
-  const [isRealTime,setIsRealTime] = useState(false);
-  const[profileformdata,setProfileFormdata]=useState({
-    name:'',
-    profileImage:'',
-    userId:''
-  })
+  // const [isRealTime,setIsRealTime] = useState(false);
+  // const[profileformdata,setProfileFormdata]=useState({
+  //   name:'',
+  //   profileImage:'',
+  //   userId:''
+  // })
 
-  
-  
-   
   return (
     <>
-    {/* <Router>
+      {/* <Router>
       <Routes>
         <Route element={<AdminLogin/>} path="/adminlogin"></Route>
         <Route element={<AdminSignUp/>} path="/adminsignup"></Route>
@@ -53,64 +48,60 @@ function App() {
         <Route element={<ErrorPage />} path="*"></Route>
       </Routes>
     </Router> */}
-    {/* <LoginContext.Provider value={{profileformdata,setProfileFormdata}}>
+      {/* <LoginContext.Provider value={{profileformdata,setProfileFormdata}}>
     <RealDataContext.Provider value={{isRealTime,setIsRealTime}}> */}
       <Router>
         <Routes>
-        <Route element={<AdminLogin/>} path="/adminlogin"></Route>
-        <Route element={<AdminSignUp/>} path="/adminsignup"></Route>
-        <Route element={<AdminDashboard/>} exact path="/admindashboard"></Route>
-        {/* <Route element={<ErrorPage />} path="*"></Route> */}
-          <Route element={<RedirectRoute />} path="/">
+          <Route element={<AdminRedirect />} path="/adminlogin">
+            <Route element={<AdminLogin />} path="/adminlogin"></Route>
+          </Route>
 
+          <Route element={<AdminSignUp />} path="/adminsignup"></Route>
+
+          <Route element={<AdminProtect />} path="/admindashboard">
+            <Route
+              element={<AdminDashboard />}
+              path="/admindashboard"
+            ></Route>
+          </Route>
+
+          {/* <Route element={<ErrorPage />} path="*"></Route> */}
+          <Route element={<RedirectRoute />} path="/">
             <Route element={<SignIn />} path="/"></Route>
-            
           </Route>
           <Route element={<SignUp />} path="/signup"></Route>
           <Route element={<EmployeeTable />} path="/table"></Route>
           {/* <Route element={<GetUserTimesheet/>} path="/getTimesheet"></Route>
           <Route element={<GetRmTimesheet/>} path="/viewTime"></Route>
           <Route element={<EditReporingTimeSheet/>} path="/editTime"></Route> */}
-         
+
           {/* for hierarchy testing */}
-          
+
           <Route element={<ProtectRoute />} path="/dashboard">
-            
-            
             <Route element={<Dashboard />} path="/dashboard">
-              
               <Route element={<Home />} path="" />
               <Route element={<LeaveForm />} path="leave" />
               <Route element={<GetRequest />} path="getRequest" />
-              <Route element={<EditRequest/>} path="editrequest/:id" />
+              <Route element={<EditRequest />} path="editrequest/:id" />
               <Route element={<ViewRequest />} path="viewRequest" />
               <Route element={<GetProfile />} path="getProfile/" />
               <Route element={<SearchProfile />} path="searchprofile/:id" />
               <Route element={<Profile />} path="profile" />
-              <Route element={<GetUserTimesheet/>} path="getTimesheet"/>
-              <Route element={<GetRmTimesheet/>} path="viewTime"/>
-              <Route element={<EditReporingTimeSheet/>} path="editTime/:id"/>
+              <Route element={<GetUserTimesheet />} path="getTimesheet" />
+              <Route element={<GetRmTimesheet />} path="viewTime" />
+              <Route element={<EditReporingTimeSheet />} path="editTime/:id" />
               <Route element={<TimesheetForm />} path="timesheetform"></Route>
-              <Route element={<Gethierarchy/>} path="chart" />
-              
-              
+              <Route element={<Gethierarchy />} path="chart" />
             </Route>
-             
-            
-
           </Route>
 
-         
           <Route element={<ProtectRoute />} path="/leaverequest">
             <Route element={<WFHform />} path="/leaverequest"></Route>
           </Route>
 
-          
-
           {/* <Route element={<DescriptionForm />} path="/testkaruna"></Route> */}
-          
-          <Route element={<ErrorPage />} path="*"></Route>
 
+          <Route element={<ErrorPage />} path="*"></Route>
         </Routes>
       </Router>
       {/* </RealDataContext.Provider>
